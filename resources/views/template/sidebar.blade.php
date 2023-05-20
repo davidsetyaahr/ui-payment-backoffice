f7ba6f621358ce03686998630592fb925b0d749b
Galih Bagus Prakasa <47969692+bagusprakasa@users.noreply.github.com>
2023-05-20 14:04:38 +0700

    Update sidebar.blade.php

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -1,159 +1,165 @@
-<div class="sidebar sidebar-style-2">
-    <div class="sidebar-wrapper scrollbar scrollbar-inner">
-        <div class="sidebar-content">
-            <div class="user">
-                <div class="avatar-sm float-left mr-2">
-                    <img src="../assets/img/profile.png" alt="..." class="avatar-img rounded-circle">
-                </div>
-                <div class="info">
-                    <a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
-                        <span>
-                            <span class="user-level">
+@if (Auth::check())
+    <div class="sidebar sidebar-style-2">
+        <div class="sidebar-wrapper scrollbar scrollbar-inner">
+            <div class="sidebar-content">
+                <div class="user">
+                    <div class="avatar-sm float-left mr-2">
+                        <img src="../assets/img/profile.png" alt="..." class="avatar-img rounded-circle">
+                    </div>
+                    <div class="info">
+                        <a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
+                            <span>
+                                <span class="user-level">
 
-                                {{ (Auth::guard('teacher')->user() != null ? Auth::guard('teacher')->user()->name : Auth::guard('staff')->user()->name) ?? '' }}
-                            </span>
-                    </a>
-
-                </div>
-            </div>
-            <ul class="nav nav-primary">
-                @if (Auth::guard('teacher')->user() == null)
-                    <li class="nav-item {{ Request::segment(1) == 'dashboard' ? 'active' : '' }}">
-                        <a href="{{ url('/dashboard') }}" class="collapsed">
-                            <i class="fas fa-home"></i>
-                            <p>Dashboard</p>
+                                    {{ (Auth::guard('teacher')->user() != null ? Auth::guard('teacher')->user()->name : Auth::guard('staff')->user()->name) ?? '' }}
+                                </span>
                         </a>
 
-                    </li>
-                @endif
-                <li
-                    class="nav-section {{ Request::segment(1) == 'admin' || Request::segment(1) == 'petani' ? 'active' : '' }}">
-                    <span class="sidebar-mini-icon">
-                        <i class="fa fa-ellipsis-h"></i>
-                    </span>
-                    <h4 class="text-section">Menu</h4>
-                </li>
+                    </div>
+                </div>
+                <ul class="nav nav-primary">
+                    @if (Auth::guard('teacher')->user() == null)
+                        <li class="nav-item {{ Request::segment(1) == 'dashboard' ? 'active' : '' }}">
+                            <a href="{{ url('/dashboard') }}" class="collapsed">
+                                <i class="fas fa-home"></i>
+                                <p>Dashboard</p>
+                            </a>
 
-
-                <li class="nav-item {{ Request::segment(1) == 'user' ? 'active' : '' }}">
-                    <a href="{{ url('/attendance/class') }}" class="collapsed">
-                        <i class="fas fa-book"></i>
-                        <p>Attendance</p>
-                    </a>
-
-                </li>
-                <li class="nav-item {{ Request::segment(1) == 'score' ? 'active' : '' }}">
-                    <a href="{{ url('/score/form') }}" class="collapsed">
-                        <i class="fas fa-user-alt"></i>
-                        <p>Student Score</p>
-                    </a>
-
-                </li>
-
-                @if (Auth::guard('teacher')->user() == null)
+                        </li>
+                    @endif
                     <li
-                        class="nav-item {{ Request::segment(1) == 'advertise' || Request::segment(1) == 'announces' || Request::segment(1) == 'pointCategories' || Request::segment(1) == 'reedemItems' || Request::segment(1) == 'testMaster' || Request::segment(1) == 'tests' || Request::segment(1) == 'parents' ? 'active' : '' }} submenu">
-                        <a data-toggle="collapse" href="#tables">
-                            <i class="fas fa-th-large"></i>
-                            <p>Master Data</p>
-                            <span class="caret"></span>
-                        </a>
-                        <div class="collapse {{ Request::segment(1) == 'advertise' || Request::segment(1) == 'announces' || Request::segment(1) == 'pointCategories' || Request::segment(1) == 'reedemItems' || Request::segment(1) == 'testMaster' || Request::segment(1) == 'tests' || Request::segment(1) == 'parents' ? 'show' : '' }}"
-                            id="tables">
-                            <ul class="nav nav-collapse">
-                                <li class="{{ Request::segment(1) == 'parents' ? 'active' : '' }}">
-                                    <a href="{{ url('/parents') }}">
-                                        <span class="sub-item ">Parents</span>
-                                    </a>
-                                </li>
-                                <li class="{{ Request::segment(1) == 'tests' ? 'active' : '' }}">
-                                    <a href="{{ url('/tests') }}">
-                                        <span class="sub-item ">Test</span>
-                                    </a>
-                                </li>
-                                <li class="{{ Request::segment(1) == 'pointCategories' ? 'active' : '' }}">
-                                    <a href="{{ url('/pointCategories') }}">
-                                        <span class="sub-item ">Point Categories</span>
-                                    </a>
-                                </li>
-                                <li class="{{ Request::segment(1) == 'reedemItems' ? 'active' : '' }}">
-                                    <a href="{{ url('/reedemItems') }}">
-                                        <span class="sub-item ">Reedem Item</span>
-                                    </a>
-                                </li>
-                                <li class="{{ Request::segment(1) == 'announces' ? 'active' : '' }}">
-                                    <a href="{{ url('/announces') }}">
-                                        <span class="sub-item ">Announcement</span>
-                                    </a>
-                                </li>
-                                <li class="{{ Request::segment(1) == 'advertise' ? 'active' : '' }}">
-                                    <a href="{{ url('/advertise') }}">
-                                        <span class="sub-item ">Advertise</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+                        class="nav-section {{ Request::segment(1) == 'admin' || Request::segment(1) == 'petani' ? 'active' : '' }}">
+                        <span class="sidebar-mini-icon">
+                            <i class="fa fa-ellipsis-h"></i>
+                        </span>
+                        <h4 class="text-section">Menu</h4>
                     </li>
-                    <li class="nav-item {{ Request::segment(1) == 'reedemPoint' ? 'active' : '' }}">
-                        <a href="{{ url('/reedemPoint') }}" class="collapsed">
-                            <i class="fas fa-download"></i>
-                            <p>Reedem Point</p>
-                        </a>
 
-                    </li>
-                    <li class="nav-item {{ Request::segment(1) == 'schedule-class' ? 'active' : '' }}">
-                        <a href="{{ url('/schedule-class') }}" class="collapsed">
-                            <i class="fas fa-calendar"></i>
-                            <p>Schedule Class</p>
-                        </a>
 
-                    </li>
-                    <li class="nav-item {{ Request::segment(2) == 'reminder' ? 'active' : '' }}">
-                        <a href="{{ url('/attendance/reminder') }}" class="collapsed">
-                            <i class="fas fa-bell"></i>
-                            <p>Reminder</p>
-                        </a>
-
-                    </li>
-                    <li class="nav-item {{ Request::segment(1) == 'mutasi' ? 'active' : '' }}">
-                        <a href="{{ url('/mutasi') }}" class="collapsed">
-                            <i class="fas fa-users"></i>
-                            <p>Jump Level</p>
-                        </a>
-
-                    </li>
-                    <li class="nav-item {{ Request::segment(1) == 'history-test' ? 'active' : '' }}">
-                        <a href="{{ url('/history-test') }}" class="collapsed">
+                    <li class="nav-item {{ Request::segment(1) == 'user' ? 'active' : '' }}">
+                        <a href="{{ url('/attendance/class') }}" class="collapsed">
                             <i class="fas fa-book"></i>
-                            <p>History Test</p>
+                            <p>Attendance</p>
                         </a>
 
                     </li>
-                @endif
-                <li class="nav-item {{ Request::segment(1) == 'saldo-awal' ? 'active' : '' }}">
-                    <a href="{{ url('/saldo-awal') }}" class="collapsed">
-                        <i class="fas fa-upload"></i>
-                        <p>Opening Balance</p>
-                    </a>
-
-                </li>
-                <li class="nav-item {{ Request::segment(1) == 'history-point' ? 'active' : '' }}">
-                    <a href="{{ url('/history-point') }}" class="collapsed">
-                        <i class="fas fa-book"></i>
-                        <p>History Point</p>
-                    </a>
-
-                </li>
-                @if (Auth::guard('teacher')->user() == null)
-                    <li class="nav-item {{ Request::segment(1) == 'barcode-student' ? 'active' : '' }}">
-                        <a href="{{ url('/barcode-student') }}" class="collapsed">
-                            <i class="fas fa-barcode"></i>
-                            <p>Barcode Student</p>
+                    <li class="nav-item {{ Request::segment(1) == 'score' ? 'active' : '' }}">
+                        <a href="{{ url('/score/form') }}" class="collapsed">
+                            <i class="fas fa-user-alt"></i>
+                            <p>Student Score</p>
                         </a>
 
                     </li>
-                @endif
-            </ul>
+
+                    @if (Auth::guard('teacher')->user() == null)
+                        <li
+                            class="nav-item {{ Request::segment(1) == 'advertise' || Request::segment(1) == 'announces' || Request::segment(1) == 'pointCategories' || Request::segment(1) == 'reedemItems' || Request::segment(1) == 'testMaster' || Request::segment(1) == 'tests' || Request::segment(1) == 'parents' ? 'active' : '' }} submenu">
+                            <a data-toggle="collapse" href="#tables">
+                                <i class="fas fa-th-large"></i>
+                                <p>Master Data</p>
+                                <span class="caret"></span>
+                            </a>
+                            <div class="collapse {{ Request::segment(1) == 'advertise' || Request::segment(1) == 'announces' || Request::segment(1) == 'pointCategories' || Request::segment(1) == 'reedemItems' || Request::segment(1) == 'testMaster' || Request::segment(1) == 'tests' || Request::segment(1) == 'parents' ? 'show' : '' }}"
+                                id="tables">
+                                <ul class="nav nav-collapse">
+                                    <li class="{{ Request::segment(1) == 'parents' ? 'active' : '' }}">
+                                        <a href="{{ url('/parents') }}">
+                                            <span class="sub-item ">Parents</span>
+                                        </a>
+                                    </li>
+                                    <li class="{{ Request::segment(1) == 'tests' ? 'active' : '' }}">
+                                        <a href="{{ url('/tests') }}">
+                                            <span class="sub-item ">Test</span>
+                                        </a>
+                                    </li>
+                                    <li class="{{ Request::segment(1) == 'pointCategories' ? 'active' : '' }}">
+                                        <a href="{{ url('/pointCategories') }}">
+                                            <span class="sub-item ">Point Categories</span>
+                                        </a>
+                                    </li>
+                                    <li class="{{ Request::segment(1) == 'reedemItems' ? 'active' : '' }}">
+                                        <a href="{{ url('/reedemItems') }}">
+                                            <span class="sub-item ">Reedem Item</span>
+                                        </a>
+                                    </li>
+                                    <li class="{{ Request::segment(1) == 'announces' ? 'active' : '' }}">
+                                        <a href="{{ url('/announces') }}">
+                                            <span class="sub-item ">Announcement</span>
+                                        </a>
+                                    </li>
+                                    <li class="{{ Request::segment(1) == 'advertise' ? 'active' : '' }}">
+                                        <a href="{{ url('/advertise') }}">
+                                            <span class="sub-item ">Advertise</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item {{ Request::segment(1) == 'reedemPoint' ? 'active' : '' }}">
+                            <a href="{{ url('/reedemPoint') }}" class="collapsed">
+                                <i class="fas fa-download"></i>
+                                <p>Reedem Point</p>
+                            </a>
+
+                        </li>
+                        <li class="nav-item {{ Request::segment(1) == 'schedule-class' ? 'active' : '' }}">
+                            <a href="{{ url('/schedule-class') }}" class="collapsed">
+                                <i class="fas fa-calendar"></i>
+                                <p>Schedule Class</p>
+                            </a>
+
+                        </li>
+                        <li class="nav-item {{ Request::segment(2) == 'reminder' ? 'active' : '' }}">
+                            <a href="{{ url('/attendance/reminder') }}" class="collapsed">
+                                <i class="fas fa-bell"></i>
+                                <p>Reminder</p>
+                            </a>
+
+                        </li>
+                        <li class="nav-item {{ Request::segment(1) == 'mutasi' ? 'active' : '' }}">
+                            <a href="{{ url('/mutasi') }}" class="collapsed">
+                                <i class="fas fa-users"></i>
+                                <p>Jump Level</p>
+                            </a>
+
+                        </li>
+                        <li class="nav-item {{ Request::segment(1) == 'history-test' ? 'active' : '' }}">
+                            <a href="{{ url('/history-test') }}" class="collapsed">
+                                <i class="fas fa-book"></i>
+                                <p>History Test</p>
+                            </a>
+
+                        </li>
+                    @endif
+                    <li class="nav-item {{ Request::segment(1) == 'saldo-awal' ? 'active' : '' }}">
+                        <a href="{{ url('/saldo-awal') }}" class="collapsed">
+                            <i class="fas fa-upload"></i>
+                            <p>Opening Balance</p>
+                        </a>
+
+                    </li>
+                    <li class="nav-item {{ Request::segment(1) == 'history-point' ? 'active' : '' }}">
+                        <a href="{{ url('/history-point') }}" class="collapsed">
+                            <i class="fas fa-book"></i>
+                            <p>History Point</p>
+                        </a>
+
+                    </li>
+                    @if (Auth::guard('teacher')->user() == null)
+                        <li class="nav-item {{ Request::segment(1) == 'barcode-student' ? 'active' : '' }}">
+                            <a href="{{ url('/barcode-student') }}" class="collapsed">
+                                <i class="fas fa-barcode"></i>
+                                <p>Barcode Student</p>
+                            </a>
+
+                        </li>
+                    @endif
+                </ul>
+            </div>
         </div>
     </div>
-</div>
+@else
+    <script>
+        window.location = "/";
+    </script>
+@endif
