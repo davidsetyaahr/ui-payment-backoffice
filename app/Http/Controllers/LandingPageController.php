00237951a8245fec109c85c9a019bf64f933f112
bagusprakasa <47969692+bagusprakasa@users.noreply.github.com>
2023-07-24 14:10:36 +0700

    updatge

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -64,65 +64,69 @@ class LandingPageController extends Controller
     {
         try {
             $student = Students::with('teacher')->find($id);
-            $class = Price::find($student->priceid);
-            $score1 = StudentScore::where('student_id', $id)->where('price_id', $class->id)->where('test_id', 1)->first();
-            if ($score1) {
-                $writing1 = StudentScoreDetail::where('student_score_id', $score1->id)->where('test_item_id', 1)->first();
-                $speaking1 = StudentScoreDetail::where('student_score_id', $score1->id)->where('test_item_id', 2)->first();
-                $reading1 = StudentScoreDetail::where('student_score_id', $score1->id)->where('test_item_id', 3)->first();
-                $listening1 = StudentScoreDetail::where('student_score_id', $score1->id)->where('test_item_id', 4)->first();
-                $grammar1 = StudentScoreDetail::where('student_score_id', $score1->id)->where('test_item_id', 5)->first();
-                $vocabulary1 = StudentScoreDetail::where('student_score_id', $score1->id)->where('test_item_id', 6)->first();
-                $average_score1 = $score1->average_score;
-            } else {
-                $writing1 = 0;
-                $speaking1 = 0;
-                $reading1 = 0;
-                $listening1 = 0;
-                $grammar1 = 0;
-                $vocabulary1 = 0;
-                $average_score1 = 0;
-            }
-            // 2
-            $score2 = StudentScore::where('student_id', $id)->where('price_id', $class->id)->where('test_id', 2)->first();
-            if ($score2) {
-                $writing2 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 1)->first();
-                $speaking2 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 2)->first();
-                $reading2 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 3)->first();
-                $listening2 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 4)->first();
-                $grammar2 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 5)->first();
-                $vocabulary2 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 6)->first();
-                $average_score2 = $score2->average_score;
-            } else {
-                $writing2 = 0;
-                $speaking2 = 0;
-                $reading2 = 0;
-                $listening2 = 0;
-                $grammar2 = 0;
-                $vocabulary2 = 0;
-                $average_score2 = 0;
-            }
+            if ($student->is_certificate == true) {
+                $class = Price::find($student->priceid);
+                $score1 = StudentScore::where('student_id', $id)->where('price_id', $class->id)->where('test_id', 1)->first();
+                if ($score1) {
+                    $writing1 = StudentScoreDetail::where('student_score_id', $score1->id)->where('test_item_id', 1)->first();
+                    $speaking1 = StudentScoreDetail::where('student_score_id', $score1->id)->where('test_item_id', 2)->first();
+                    $reading1 = StudentScoreDetail::where('student_score_id', $score1->id)->where('test_item_id', 3)->first();
+                    $listening1 = StudentScoreDetail::where('student_score_id', $score1->id)->where('test_item_id', 4)->first();
+                    $grammar1 = StudentScoreDetail::where('student_score_id', $score1->id)->where('test_item_id', 5)->first();
+                    $vocabulary1 = StudentScoreDetail::where('student_score_id', $score1->id)->where('test_item_id', 6)->first();
+                    $average_score1 = $score1->average_score;
+                } else {
+                    $writing1 = 0;
+                    $speaking1 = 0;
+                    $reading1 = 0;
+                    $listening1 = 0;
+                    $grammar1 = 0;
+                    $vocabulary1 = 0;
+                    $average_score1 = 0;
+                }
+                // 2
+                $score2 = StudentScore::where('student_id', $id)->where('price_id', $class->id)->where('test_id', 2)->first();
+                if ($score2) {
+                    $writing2 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 1)->first();
+                    $speaking2 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 2)->first();
+                    $reading2 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 3)->first();
+                    $listening2 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 4)->first();
+                    $grammar2 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 5)->first();
+                    $vocabulary2 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 6)->first();
+                    $average_score2 = $score2->average_score;
+                } else {
+                    $writing2 = 0;
+                    $speaking2 = 0;
+                    $reading2 = 0;
+                    $listening2 = 0;
+                    $grammar2 = 0;
+                    $vocabulary2 = 0;
+                    $average_score2 = 0;
+                }
 
-            // 3
-            $score3 = StudentScore::where('student_id', $id)->where('price_id', $class->id)->where('test_id', 3)->first();
-            if ($score3) {
-                $writing3 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 1)->first();
-                $speaking3 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 2)->first();
-                $reading3 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 3)->first();
-                $listening3 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 4)->first();
-                $grammar3 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 5)->first();
-                $vocabulary3 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 6)->first();
-                $average_score3 = $score3->average_score;
+                // 3
+                $score3 = StudentScore::where('student_id', $id)->where('price_id', $class->id)->where('test_id', 3)->first();
+                if ($score3) {
+                    $writing3 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 1)->first();
+                    $speaking3 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 2)->first();
+                    $reading3 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 3)->first();
+                    $listening3 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 4)->first();
+                    $grammar3 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 5)->first();
+                    $vocabulary3 = StudentScoreDetail::where('student_score_id', $score2->id)->where('test_item_id', 6)->first();
+                    $average_score3 = $score3->average_score;
+                } else {
+                    $writing3 = 0;
+                    $speaking3 = 0;
+                    $reading3 = 0;
+                    $listening3 = 0;
+                    $grammar3 = 0;
+                    $vocabulary3 = 0;
+                    $average_score3 = 0;
+                }
+                return view('landing-page.e-certificate', compact('student', 'class', 'score1', 'writing1', 'speaking1', 'reading1', 'listening1', 'grammar1', 'vocabulary1', 'average_score1', 'score2', 'writing2', 'speaking2', 'reading2', 'listening2', 'grammar2', 'vocabulary2', 'average_score2', 'score3', 'writing3', 'speaking3', 'reading3', 'listening3', 'grammar3', 'vocabulary3', 'average_score3', 'id'));
             } else {
-                $writing3 = 0;
-                $speaking3 = 0;
-                $reading3 = 0;
-                $listening3 = 0;
-                $grammar3 = 0;
-                $vocabulary3 = 0;
-                $average_score3 = 0;
+                return view('landing-page.not-found', compact('id'));
             }
-            return view('landing-page.e-certificate', compact('student', 'class', 'score1', 'writing1', 'speaking1', 'reading1', 'listening1', 'grammar1', 'vocabulary1', 'average_score1', 'score2', 'writing2', 'speaking2', 'reading2', 'listening2', 'grammar2', 'vocabulary2', 'average_score2', 'score3', 'writing3', 'speaking3', 'reading3', 'listening3', 'grammar3', 'vocabulary3', 'average_score3', 'id'));
         } catch (\Throwable $th) {
             throw $th;
         }
