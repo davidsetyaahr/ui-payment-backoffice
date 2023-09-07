4e0e9ed2024dfd39ca34d38fd484bbedf011e7df
bagusprakasa <galihbagus555@gmail.com>
2023-09-07 15:01:46 +0700

    Update index.blade.php

--- resources/views/attendance/index.blade.php
+++ resources/views/attendance/index.blade.php
@@ -181,87 +181,92 @@
                         <div class="card-body">
                             <div class="row">
                                 @foreach ($private as $key => $item)
-                                    <div class="col-sm-6 col-md-4 ">
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <span style="font-size: 16px">
-                                                    <div class="d-flex justify-content-between">
-                                                        <div>
-                                                            @php
-                                                                $studentName = DB::table('student')
-                                                                    ->where('priceid', $item->priceid)
-                                                                    ->where('day1', $item->day1)
-                                                                    ->where('day2', $item->day2)
-                                                                    ->where('id_teacher', $item->id_teacher)
-                                                                    ->where('course_time', $item->course_time)
-                                                                    ->first();
-                                                            @endphp
-                                                            <b> {{ ucwords($studentName->name) }}</b>
-                                                            <br>
-                                                        </div>
-                                                        @if (Auth::guard('staff')->check() == true)
+                                    @php
+                                        $studentName = DB::table('student')
+                                            ->where('priceid', $item->priceid)
+                                            ->where('day1', $item->day1)
+                                            ->where('day2', $item->day2)
+                                            ->where('id_teacher', $item->id_teacher)
+                                            ->where('course_time', $item->course_time)
+                                            ->groupBy('id')
+                                            ->get();
+                                    @endphp
+                                    @foreach ($studentName as $keyStudentName => $itemStudentName)
+                                        <div class="col-sm-6 col-md-4 ">
+                                            <div class="card">
+                                                <div class="card-body">
+                                                    <span style="font-size: 16px">
+                                                        <div class="d-flex justify-content-between">
                                                             <div>
-                                                                <form action="{{ url('schedule-class/delete') }}"
-                                                                    method="POST" class="form-inline">
-                                                                    @method('delete')
-                                                                    @csrf
-                                                                    <input type="hidden" name="priceid"
-                                                                        value="{{ $item->priceid }}">
-                                                                    <input type="hidden" name="day1"
-                                                                        value="{{ $item->day1 }}">
-                                                                    <input type="hidden" name="day2"
-                                                                        value="{{ $item->day2 }}">
-                                                                    <input type="hidden" name="course_time"
-                                                                        value="{{ $item->course_time }}">
-                                                                    <input type="hidden" name="id_teacher"
-                                                                        value="{{ $item->id_teacher }}">
-                                                                    <button type="submit"
-                                                                        onclick="return confirm('apakah anda yakin ingin menghapus data ??')"
-                                                                        class="btn btn-xs btn-danger"
-                                                                        style="margin-right: 10px !important;"><i
-                                                                            class="fas fa-trash"></i></button>
-                                                                    <a href="javascript:void(0)"
-                                                                        class="btn btn-xs btn-success" data-toggle="modal"
-                                                                        data-target="#editJadwalModal"
-                                                                        onclick="updateModalPrv({{ $key }})"><i
-                                                                            class="fas fa-pencil-alt"></i></a>
-                                                                </form>
+                                                                <b> {{ ucwords($itemStudentName->name) }}</b>
+                                                                <br>
                                                             </div>
-                                                        @endif
-                                                    </div> <i class="fa fas fa-angle-right"></i>
-                                                    <b>{{ $item->day_one }} {{ $item->day1 != $item->day2 ? '&' : '' }}
-                                                        {{ $item->day1 != $item->day2 ? $item->day_two : '' }}</b>
-                                                    <br>
-                                                    <b>{{ $item->course_time }}</b>
+                                                            @if (Auth::guard('staff')->check() == true)
+                                                                <div>
+                                                                    <form action="{{ url('schedule-class/delete') }}"
+                                                                        method="POST" class="form-inline">
+                                                                        @method('delete')
+                                                                        @csrf
+                                                                        <input type="hidden" name="priceid"
+                                                                            value="{{ $item->priceid }}">
+                                                                        <input type="hidden" name="day1"
+                                                                            value="{{ $item->day1 }}">
+                                                                        <input type="hidden" name="day2"
+                                                                            value="{{ $item->day2 }}">
+                                                                        <input type="hidden" name="course_time"
+                                                                            value="{{ $item->course_time }}">
+                                                                        <input type="hidden" name="id_teacher"
+                                                                            value="{{ $item->id_teacher }}">
+                                                                        <button type="submit"
+                                                                            onclick="return confirm('apakah anda yakin ingin menghapus data ??')"
+                                                                            class="btn btn-xs btn-danger"
+                                                                            style="margin-right: 10px !important;"><i
+                                                                                class="fas fa-trash"></i></button>
+                                                                        <a href="javascript:void(0)"
+                                                                            class="btn btn-xs btn-success"
+                                                                            data-toggle="modal"
+                                                                            data-target="#editJadwalModal"
+                                                                            onclick="updateModalPrv({{ $key }})"><i
+                                                                                class="fas fa-pencil-alt"></i></a>
+                                                                    </form>
+                                                                </div>
+                                                            @endif
+                                                        </div> <i class="fa fas fa-angle-right"></i>
+                                                        <b>{{ $item->day_one }}
+                                                            {{ $item->day1 != $item->day2 ? '&' : '' }}
+                                                            {{ $item->day1 != $item->day2 ? $item->day_two : '' }}</b>
+                                                        <br>
+                                                        <b>{{ $item->course_time }}</b>
 
-                                                    <input type="hidden" id="prvprogramModal{{ $key }}"
-                                                        value="{{ $item->program }}">
-                                                    <input type="hidden" id="prvcourseTimeModal{{ $key }}"
-                                                        value="{{ $item->course_time }}">
-                                                    <input type="hidden" id="prvday1Modal{{ $key }}"
-                                                        value="{{ $item->day1 }}">
-                                                    <input type="hidden" id="prvday2Modal{{ $key }}"
-                                                        value="{{ $item->day2 }}">
-                                                    <input type="hidden" id="prvteacherModal{{ $key }}"
-                                                        value="{{ $item->teacher_name }}">
-                                                    <input type="hidden" id="prvclassModal{{ $key }}"
-                                                        value="{{ $item->priceid }}">
-                                                    <input type="hidden" id="prvdayOneModal{{ $key }}"
-                                                        value="{{ $item->day_one }}">
-                                                    <input type="hidden" id="prvdayTwoModal{{ $key }}"
-                                                        value="{{ $item->day_two }}">
-                                                    <input type="hidden" id="prvidteacherModal{{ $key }}"
-                                                        value="{{ $item->id_teacher }}">
-                                                </span>
+                                                        <input type="hidden" id="prvprogramModal{{ $key }}"
+                                                            value="{{ $item->program }}">
+                                                        <input type="hidden" id="prvcourseTimeModal{{ $key }}"
+                                                            value="{{ $item->course_time }}">
+                                                        <input type="hidden" id="prvday1Modal{{ $key }}"
+                                                            value="{{ $item->day1 }}">
+                                                        <input type="hidden" id="prvday2Modal{{ $key }}"
+                                                            value="{{ $item->day2 }}">
+                                                        <input type="hidden" id="prvteacherModal{{ $key }}"
+                                                            value="{{ $item->teacher_name }}">
+                                                        <input type="hidden" id="prvclassModal{{ $key }}"
+                                                            value="{{ $item->priceid }}">
+                                                        <input type="hidden" id="prvdayOneModal{{ $key }}"
+                                                            value="{{ $item->day_one }}">
+                                                        <input type="hidden" id="prvdayTwoModal{{ $key }}"
+                                                            value="{{ $item->day_two }}">
+                                                        <input type="hidden" id="prvidteacherModal{{ $key }}"
+                                                            value="{{ $item->id_teacher }}">
+                                                    </span>
 
-                                                <div class="d-flex justify-content-between mt-4">
-                                                    <div class="fw-bold">{{ $item->teacher_name }}</div>
-                                                    <a href="{{ url('attendance/form/' . $item->priceid . '?day1=' . $item->day1 . '&day2=' . $item->day2 . '&time=' . $item->course_time . '&teacher=' . $item->id_teacher . '&new=' . $item->is_class_new . '&student=') }}"
-                                                        class="btn btn-xs btn-primary">View</a>
+                                                    <div class="d-flex justify-content-between mt-4">
+                                                        <div class="fw-bold">{{ $item->teacher_name }}</div>
+                                                        <a href="{{ url('attendance/form/' . $item->priceid . '?day1=' . $item->day1 . '&day2=' . $item->day2 . '&time=' . $item->course_time . '&teacher=' . $item->id_teacher . '&new=' . $item->is_class_new . '&student=' . $itemStudentName->id) }}"
+                                                            class="btn btn-xs btn-primary">View</a>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
+                                    @endforeach
                                 @endforeach
                             </div>
                         </div>
