7a8a9f65e854c8b72f4275b64702118d78849356
bagusprakasa <galihbagus555@gmail.com>
2023-09-16 12:44:27 +0700

    Update Attendance.php

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -10,7 +10,7 @@ class Attendance extends Model
     public $timestamps = false;
     use HasFactory;
     protected $table = 'attendances';
-    protected $fillable = ['price_id', 'day1', 'day2', 'course_time', 'date', 'teacher_id', 'activity', 'text_book', 'excercise_book', 'is_presence', 'id_test', 'date_review', 'date_test'];
+    protected $fillable = ['price_id', 'day1', 'day2', 'course_time', 'date', 'teacher_id', 'activity', 'text_book', 'excercise_book', 'is_presence', 'id_test', 'date_review', 'date_test', 'is_class_new'];
 
     /**
      * Get all of the detail for the Attendance
